9cca35522a23fecea50949bc086205eac96e9f90
NekrasovaTanya1 <nekrasova.tanya85@gmail.com>
2019-10-11 11:01:32 +0300

    init setting ide

--- config/app.php
+++ config/app.php
@@ -174,6 +174,11 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        
+        /*
+         * my providers
+         */
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
 
     ],
 
